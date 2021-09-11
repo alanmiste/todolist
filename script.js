05f42303a36f8fd7b93e99daa7b74aaa60c3dfc3
alanmiste <baranalaan@gmail.com>
2021-09-11 15:34:35 +0200

    JS- add show tasks function

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const addBtn = document.querySelector(".inputField button");
 const todoList = document.querySelector(".todoList");
 const deleteAllBtn = document.querySelector(".footer button");
 
+/* localStorage.setItem("name1","text text");
+localStorage.getItem("name1"); */
+
+
 // on key uo event
 inputBox.onkeyup = ()=>{
     let enteredValue = inputBox.value; // getting user entered value
@@ -15,3 +19,29 @@ inputBox.onkeyup = ()=>{
         addBtn.classList.remove("active"); // disable the add button.
     }
 }
+
+function showTasks(){
+    let getLocalStorageData = localStorage.getItem("Todo List");
+    if(getLocalStorageData == null){
+        listArr = [];
+    }else{
+        listArr = JSON.parse(getLocalStorageData); // with JSON.parse() the data becomes a JavaScript object.
+    }
+    const pendingTasksNum = document.querySelector(".pendingTasks");
+    pendingTasksNum.textContent = listArr.length; // to pass array length in pinding task.
+
+    if(listArr.length > 0){
+        deleteAllBtn.classList.add("active"); // if the array length is greater than 0 then active the delete button.
+    }else{
+        deleteAllBtn.classList.remove("active"); // disabel the delete button.
+    }
+
+    let newTag = "";
+    listArr.forEach((element, index) => { // this to create HTML element for each element in the array.
+        newTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class=fas fa-trash"></i></span></li>`;        
+    });
+    todoList.innerHTML = newTag; // adding new li tag inside ul tag.
+
+    inputBox.value = ""; // Initialize the input box after the task has been added.
+}
+
